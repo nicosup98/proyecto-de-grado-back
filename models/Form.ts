b4f3b7b455903c76d4387cd1a01241892bfafd0a
nicolangelo damico <damiconicola98@gmail.com>
2024-09-23 13:03:47 -0400

    se añadio biblioteca a Bloque

--- models/Form.ts
+++ models/Form.ts
@@ -1,55 +1,73 @@
-export type Bloque = "A" | "B" | "C" | "D" | "E" | "F" | "G" | "rectorado" | "cancha" | "Feria de comida" | "Centro de mantenimiento" | 'Vivero'
-type Persona = "estudiante" | "profesor" | "personal" | "mantenimiento" | "visitante"
-type Genero = "masculino" | "femenino" | "otro"
+export type Bloque =
+    | "A"
+    | "B"
+    | "C"
+    | "D"
+    | "E"
+    | "F"
+    | "G"
+    | "rectorado"
+    | "cancha"
+    | "Feria de comida"
+    | "Centro de mantenimiento"
+    | "Vivero"
+    | "Biblioteca";
 
-type RecordType = Record<string,any>
+
+type Persona =
+    | "estudiante"
+    | "profesor"
+    | "personal"
+    | "mantenimiento"
+    | "visitante";
+    
+type Genero = "masculino" | "femenino" | "otro";
+
+type RecordType = Record<string, any>;
 /**
  * body del formulario
  */
-export interface Form{
-    email: string
-    tiempo_lavamanos: number
-    cantidad_veces_inodoro: number
-    cantidad_veces_urinario?: number,
-    tiempo_bebedero: number
-    bloque_preferido: Bloque
-    tipo_persona: Persona
-    genero: Genero
-    punto_rojo?:PuntoRojo[]
+export interface Form {
+    email: string;
+    tiempo_lavamanos: number;
+    cantidad_veces_inodoro: number;
+    cantidad_veces_urinario?: number;
+    tiempo_bebedero: number;
+    bloque_preferido: Bloque;
+    tipo_persona: Persona;
+    genero: Genero;
+    punto_rojo?: PuntoRojo[];
 }
 /**
  * tabla de bd
  */
 export interface Consumo extends RecordType {
-    lavamano_litro_s: number
-    poceta_litro_jalada:number
-    regadera_litro_s: number
-    bebedero_litro_s: number
-    manguera_litro_s: number //mirar bien el nombre
-    urinario_litro_jalada: number
-    create_at: string
-    updated_at: string
-} 
-
-
-
+    lavamano_litro_s: number;
+    poceta_litro_jalada: number;
+    regadera_litro_s: number;
+    bebedero_litro_s: number;
+    manguera_litro_s: number; //mirar bien el nombre
+    urinario_litro_jalada: number;
+    create_at: string;
+    updated_at: string;
+}
 
 interface PuntoRojo {
-    nombre: string,
-    tipo: string
-    litros?: number,
-    tiempo_uso: number
+    nombre: string;
+    tipo: string;
+    litros?: number;
+    tiempo_uso: number;
 }
 
 export interface ConsumoCalculado {
-    email: string,
+    email: string;
     genero: Genero;
-    tipo_usuario: Persona
-    consumo_detalles: ConsumoDetalles,
+    tipo_usuario: Persona;
+    consumo_detalles: ConsumoDetalles;
     consumo_total: {
-        semanal: number,
-        mensual: number
-    }
+        semanal: number;
+        mensual: number;
+    };
 }
 
 export interface ConsumoDetalles {
@@ -58,6 +76,5 @@ export interface ConsumoDetalles {
     litros_bebedero: number;
     bloque_preferido: Bloque;
     litros_urinarios: number;
-    puntos_rojos: number
-
+    puntos_rojos: number;
 }
